b38491e5bc02cbd469b95ed3a08d1d5fe30d7d6d
Eduardo Flores <edfloreshz@gmail.com>
2021-10-23 21:08:13 -0700

    New script to add Windows option to systemd-boot in Arch Linux

---
+++ archlinux/systemd-boot-win.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+getDevices() {
+    sudo fdisk -l 2>&1 | grep EFI | grep -o "^\S*" | tail -2
+}
+
+echo "Select your Windows EFI partition:"
+windows=""
+
+select DEVICE in $(getDevices);
+do
+    case $DEVICE in
+        "$QUIT")
+            echo "Exiting"
+            break
+            ;;
+        *)
+            windows="$DEVICE"
+            break
+            ;;
+    esac
+done
+
+echo "Select your Linux EFI partition:"
+linux=""
+
+select DEVICE in $(getDevices);
+do
+    case $DEVICE in
+        "$QUIT")
+            echo "Exiting"
+            break
+            ;;
+        *)
+            linux="$DEVICE"
+            break
+            ;;
+    esac
+done
+echo Windows partition set to: $windows
+echo Linux partition set to: $linux
+
+if [ ! -d /mnt/win ] || [ ! -d /mnt/lnx ] 
+then
+    sudo mkdir /mnt/{win,lnx}
+fi
+sudo mount /mnt/win $windows
+sudo mount /mnt/lnx $linux
+sudo cp -R /mnt/win/EFI/Microsoft /mnt/lnx/EFI
+
+echo Done! Now you can reboot...
