c21e4b8ee1dab5d5ef87221e95d016732ea67613
Eduardo Flores <edfloreshz@gmail.com>
2021-10-26 11:27:00 -0700

    Fixed archconfig.sh

--- archlinux/archconfig.sh
+++ archlinux/archconfig.sh
@@ -21,7 +21,7 @@ fi
 #fi
 
 # Paru installation
-if ! command -v paru &>/dev/null
+if ! type paru 2>&1 1>/dev/null
 then
     sudo pacman -S --needed --noconfirm base-devel git
     git clone https://aur.archlinux.org/paru.git
@@ -29,40 +29,59 @@ then
     makepkg -si
     cd ..
     rm -rf paru
+    echo "Paru installed"
 else
     echo "Paru is already installed."
 fi
+
+read -p "Press Enter to continue" </dev/tty
 
 # Zsh and Ohmyzsh installation
 if [ ! -d $HOME/.oh-my-zsh ]
 then
     paru -S zsh --needed --noconfirm
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    echo "Ohmyzsh installed"
 else
     echo "Ohmyzsh is already installed."
 fi
+
+read -p "Press Enter to continue" </dev/tty
 
 # Zsh configuration
 cd $HOME
 if [[ ! $( cat $HOME/.zshrc | grep gcl ) ]];
 then
     curl -L https://raw.githubusercontent.com/edfloreshz/config/main/zsh/.zshrc > .zshrc
+    echo "Zsh now configured"
 else
     echo "Zsh is already configured."
 fi
 
+read -p "Press Enter to continue" </dev/tty
+
 # Developer tools
-hash go clang cmake rustc && echo "Developer tools already installed." ||
-if [ $( paru -S go clang cmake --needed --noconfirm &>/dev/null ) ];
+hash go clang cmake rustc &>/dev/null && echo "Developer tools already installed." ||
+if [ $( paru -S go clang cmake --needed ) ];
 then
-  echo "Installing Rust..."
-  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
   echo "Installed developer tools."
 else
   echo "Some developer tools failed to install."
 fi
 
+read -p "Press Enter to continue" </dev/tty
+# Rust
+if ! type cargo 2>&1 1>/dev/null
+then
+    echo "Installing Rust..."
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    echo "Installed Rust."
+else
+    echo "Rust is already installed"
+fi
 
+
+read -p "Press Enter to continue" </dev/tty
 # Vim configuration
 cd $HOME
 
@@ -73,6 +92,8 @@ then
 else
     echo "VimPlug is already installed."
 fi
+
+read -p "Press Enter to continue" </dev/tty
 if [ ! -d $HOME/.vim/plugged ]
 then
     vim +PlugInstall +qall
@@ -82,12 +103,9 @@ else
     echo "Vim plugins are already installed"
 fi
 
+read -p "Press Enter to continue" </dev/tty
 # Packages installation
 echo "Installing packages..."
-hash telegram-desktop jetbrains-toolbox cmake code discord timeshift spotify && echo "All programs installed" ||
-if [[ $( paru -S telegram-desktop jetbrains-toolbox cmake visual-studio-code-bin discord timeshift spotify --needed &>/dev/null ) ]];
-then
-    echo "All programs have been installed."
-else
-    echo "Some programs were omitted or not installed.."
-fi
+hash telegram-desktop jetbrains-toolbox cmake code discord timeshift spotify &>/dev/null && echo "All programs installed" || paru -S telegram-desktop jetbrains-toolbox cmake visual-studio-code-bin discord timeshift spotify --needed
+
+read -p "Press Enter to continue" </dev/tty
